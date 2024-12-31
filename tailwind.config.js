2fc9853897ffb3b1e791917dc681794676051b99
Houssam Ouhra <houssamblur04@gmail.com>
2024-12-31 03:10:58 +0100

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      borderWidth: {
+        10: "10px",
+        12: "12px",
+        16: "16px",
+      },
+    },
+  },
+  plugins: [],
+};
